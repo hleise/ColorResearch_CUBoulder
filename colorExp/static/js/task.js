a3bcfabb2d4b25789eb8c0960bb2c7d8051ef8e7
Hunter Leise <hleise27@gmail.com>
2016-08-09 18:23:14 -0600

    Basic javascript works in the first instructions page. Looking for a better way to integrate my code with the given project structure.

--- colorExp/static/js/task.js
+++ colorExp/static/js/task.js
@@ -24,11 +24,18 @@ var STATE;
 var ITEMLIST = ["item1", "item2", "item3"];
 var rootpath_orignals = "../static/images/stimuli/";
 
+var timer = null;
+var time = 0;
+var caseIterator = 0;
+var testSetOne = ["Takete", "Bouba", "Tikubo", "Maluma"];
+var testSetTwo = ["Tekete", "Boaba", "Takehbi", "Maloma"];
+
 /*************************
  * INSTRUCTIONS         
  *************************/
 
 var Instructions = function() {
+    resetPage();
 
     // The list of pages for this set of instructions
     this.pages = $c.instructions[STATE.experiment_phase].pages;
@@ -53,18 +60,8 @@ var Instructions = function() {
 
         this.example = this.examples[STATE.index];
 	if (this.example) {
-        var temp = this.example[0];
-        // Draw the examples
-        Draw_Image('ex-original-image', rootpath_orignals + this.example.original_filename);
-        Draw_Image('ex-item1-image', rootpath_orignals + this.example.image1_filename);
-        Draw_Image('ex-item2-image', rootpath_orignals + this.example.image2_filename);
-        Draw_Image('ex-item3-image', rootpath_orignals + this.example.image3_filename);
-        var stimuliWidth = $("#ex-item1-image").width();
-        $(".original-stimuli").width(stimuliWidth);
-        $(".original-stimuli").height(stimuliWidth);
-        $(".filler-div").width(stimuliWidth);
-        $(".filler-div").height(stimuliWidth);
-        $(".filler-div").css('line-height', "" + stimuliWidth + "px")
+        $("#option-one-text").html("Hello");
+        $("#option-two-text").html("World");
 	}
 	    
         // Bind a handler to the "next" button. We have to wrap it in
@@ -133,6 +130,7 @@ var Instructions = function() {
  *****************/
 
 var TestPhase = function() {
+    resetPage();
 
     /* Instance variables */
 
@@ -365,6 +363,131 @@ var TestPhase = function() {
         this.show();
     };
 };
+
+
+/* Returns a random color name */
+    function getRandomColor() {
+        var colors = ["Aqua","Aquamarine","Black", "BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","ForestGreen","Fuchsia","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HotPink","IndianRed","Indigo","Khaki","LawnGreen","Lime","LimeGreen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","Moccasin","NavajoWhite","Navy","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","YellowGreen"];
+        return colors[Math.floor(Math.random() * colors.length)];
+    }
+
+    /* Handles when a word option is clicked */
+    function optionClicked() {
+        setStateDisplay("recenter");
+        randomizeOptionColors();
+
+        if (caseIterator >= 3 ) {
+            caseIterator = 0;
+        } else {
+            caseIterator++;
+        }
+
+        setTestCase();
+    }
+
+    /* Sets random colors for the text and border of both name options */
+    function randomizeOptionColors() {
+        var randomColor = getRandomColor();
+        var randomDarker = Math.floor(Math.random() * 2);
+
+        $("#option-one").css("borderColor", randomColor);
+        $("#option-two").css("borderColor", randomColor);
+
+        if (randomDarker == 0) {
+            $("#option-one").css("filter", "brightness(90%)");
+            $("#option-one").css("WebkitFilter", "brightness(90%)");
+        } else {
+            $("#option-two").css("filter", "brightness(90%)");
+            $("#option-two").css("WebkitFilter", "brightness(90%)");
+        }
+    }
+
+    /* Resets the page to its defaults when the page is refreshed. */
+    function resetPage() {
+        setStateDisplay("recenter");
+        setTestCase();
+        randomizeOptionColors();
+    }
+
+    /* Changes the size of the recenter shape filling */
+    function setCenterFilling(fillingRadius) {
+        var fillingDiameter = fillingRadius * 2;
+        $("#recenter-shape-filling").css("top", (9 - fillingRadius) + "px");
+        $("#recenter-shape-filling").css("height", fillingDiameter + "px");
+        $("#recenter-shape-filling").css("width", fillingDiameter + "px");
+        $("#recenter-shape-filling").css("borderRadius", fillingRadius + "px");
+    }
+
+    /* Changes the display settings for either the "recenter" or "testCase" state */
+    function setStateDisplay(state) {
+        switch (state) {
+            case "recenter":
+                $("#case-instructions").css("display", "none");
+                $("#recenter-instructions").css("display", "block");
+                $("#recenter-shape").css("display", "block");
+
+                $("#shape-image").css("display", "none");
+                $("#option-one-container").css("display", "none");
+                $("#option-two-container").css("display", "none");
+
+                $("#options-container").css("marginTop", "185px");
+                $("#option-one").css("filter", "brightness(100%)");
+                $("#option-one").css("WebkitFilter", "brightness(100%)");
+                $("#option-two").css("filter", "brightness(100%)");
+                $("#option-two").css("WebkitFilter", "brightness(100%)");
+                
+                break;
+            case "testCase":
+                $("#case-instructions").css("display", "block");
+                $("#recenter-instructions").css("display", "none");
+                $("#recenter-shape").css("display", "none");
+
+                $("#shape-image").css("display", "inline");
+                $("#option-one-container").css("display", "block");
+                $("#option-two-container").css("display", "block");
+
+                $("#options-container").css("marginTop", "20px");
+
+                break;
+            default:
+                console.log("State sent to setStateDisplay isn't valid.")
+        }
+    }
+
+    /* Sets the image and name options given the caseIterator. */
+    function setTestCase() {
+        $("#shape-image").src = "Images/" + caseIterator + ".svg";
+        $("#option-one-text").html(testSetOne[caseIterator]);
+        $("#option-two-text").html(testSetTwo[caseIterator]);
+    }
+
+    /* Starts the timer */
+    function startTimer() {
+        if (!timer) {
+            time = 0;
+            setCenterFilling(0);
+            timer = setInterval(tick, 50);
+        }
+    }
+
+    /* Stops the timer */
+    function stopTimer() {
+        clearInterval(timer);
+        timer = null;
+        setCenterFilling(0);
+    }
+
+    /* Changes the recenter shape filling size during each tick. */
+    function tick() {
+        if (time < 10) {
+            time++;
+            setCenterFilling(time);
+        } else {
+            stopTimer();
+            setStateDisplay("testCase");
+        }
+    }
+
 
 
 // --------------------------------------------------------------------
